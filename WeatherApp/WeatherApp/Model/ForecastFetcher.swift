4a865833ccacd17e19c751385c95513fb43c7a89
Gabbrolee <akrine2christophine@gmail.com>
2021-08-11 10:06:50 +0100

    second commit

---
+++ WeatherApp/WeatherApp/Model/ForecastFetcher.swift
@@ -0,0 +1,46 @@
+//
+//  ForecastFetcher.swift
+//  WeatherApp
+//
+//  Created by user on 10/08/2021.
+//
+
+import UIKit
+class LoadCurrentData {
+  func fetchForecastData(city: String,forcast:Bool,completionHandler: @escaping (WeatherInfo)-> () ){
+    let apiKey = "28cb3dc805390fe699ccddc6ca324cef"
+   
+    let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&cnt=5&appid=\(apiKey)"
+    if let url = URL(string: urlString) {
+      URLSession.shared.dataTask(with: url) { data, response, error in
+        if let error = error {
+          print("Eror: \(error.localizedDescription)")
+        }
+
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200
+        else {
+          print("Error : HTTP Response Code Error")
+          return
+        }
+
+        guard let data = data else{
+          print("Error: No Response")
+          return
+        }
+        if (!forcast){
+          do {
+            let weatherInfo = try JSONDecoder().decode(WeatherInfo.self, from: data)
+            print("Data is loading...")
+            DispatchQueue.main.async {
+              completionHandler(weatherInfo)
+            }
+          } catch {
+            print(error.localizedDescription)
+          }
+        }else{
+        }
+      }.resume()
+    }
+
+  }
+}
---
+++ WeatherApp/WeatherApp/Model/model.swift
@@ -0,0 +1,20 @@
+//
+//  model.swift
+//  WeatherApp
+//
+//  Created by user on 10/08/2021.
+//
+
+import UIKit
+struct WeatherInfo:Codable{
+  let main: PresentWeather
+  
+}
+
+struct PresentWeather: Codable{
+  var temp: Float
+  var temp_min: Float
+  var temp_max: Float
+
+}
+
